8adfd2878137758cf84ac45dd6834aea378a7360
DmitryProshin <do.proshin@yandex.ru>
2023-03-08 17:18:05 +0500

    Прошин Дмитрий exercises02

--- exercises02/src/main/scala/exercises02/Counter.scala
+++ exercises02/src/main/scala/exercises02/Counter.scala
@@ -1,23 +1,36 @@
 package exercises02
 
+import scala.util.matching.Regex
+
 object Counter {
 
   /**
     * Посчитать количество вхождений слов в тексте
     * слово отделено символами [\s.,!?:\n\t\r]
     */
-  def countWords(text: String): Map[String, Int] = ???
+  def countWords(text: String): Map[String, Int] = {
+    text.toLowerCase.split("[\\s.,!?:()\\n\\t\\r]").filter(a => a != "").groupMapReduce(identity)(_ => 1)(_ + _)
+  }
 
   /**
     * Посчитать количество вхождений английских слов в тексте
     * слово отделено символами [\s.,!?:\n\t\r]
     */
-  def countEnglishWords(text: String): Map[String, Int] = ???
+  def countEnglishWords(text: String): Map[String, Int] = {
+    text.toLowerCase
+      .split("[\\s.,!?:()\\n\\t\\r[а-яА-ЯёЁ]]")
+      .filter(a => a != "")
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+  }
 
   /**
     * Посчитать количество вхождений чисел в тексте
     * число отделено символами [\s!?:\n\t\r]
     */
-  def countNumbers(text: String): Map[String, Int] = ???
-
+  def countNumbers(text: String): Map[String, Int] = {
+    text.toLowerCase
+      .split("[\\s.,!?:[A-Za-z]()\\n\\t\\r[а-яА-ЯёЁ]]")
+      .filter(a => a != "")
+      .groupMapReduce(identity)(_ => 1)(_ + _)
+  }
 }
