bca5fcd826c10a3c85dd2ae960fc1feaf0227e42
DmitryProshin <do.proshin@yandex.ru>
2023-03-22 19:05:55 +0500

    Прошин Дмитрий exercises03

--- exercises03/src/main/scala/exercises03/MyList.scala
+++ exercises03/src/main/scala/exercises03/MyList.scala
@@ -1,5 +1,8 @@
 package exercises03
 
+import scala.annotation.tailrec
+import scala.collection.IndexedSeqView.Reverse
+
 sealed trait MyList[+A]
 
 final case class Cons[A](head: A, tail: MyList[A]) extends MyList[A]
@@ -7,7 +10,22 @@ final case class Cons[A](head: A, tail: MyList[A]) extends MyList[A]
 case object Nil extends MyList[Nothing]
 
 object MyList {
-  def sum(list: MyList[Int]): Int = ???
+  def sum(list: MyList[Int]): Int = {
+    list match {
+      case Cons(head, tail) => head + sum(tail)
+      case Nil              => 0
+    }
+  }
 
-  def reverse[A](list: MyList[A]): MyList[A] = ???
+  def reverse[A](list: MyList[A]): MyList[A] = {
+    @tailrec
+    def reverseRec(list: MyList[A], reverseList: MyList[A]): MyList[A] = {
+      list match {
+        case Cons(head, tail) => reverseRec(tail, Cons(head, reverseList))
+        case Nil              => reverseList
+      }
+    }
+    reverseRec(list, Nil)
+  }
+
 }
--- exercises03/src/main/scala/exercises03/game/Game.scala
+++ exercises03/src/main/scala/exercises03/game/Game.scala
@@ -1,9 +1,32 @@
 package exercises03.game
 
 object Game {
-  def parseState(input: String, number: Int): State = ???
+  def parseState(input: String, number: Int): State = input.toIntOption match {
+    case Some(value) => {
+      if (value < number) {
+        NumberIsBigger
+      } else if (value > number) {
+        NumberIsSmaller
+      } else {
+        Guessed
+      }
+    }
+    case None => {
+      if (input == GameController.IGiveUp) {
+        GiveUp
+      } else {
+        WrongInput
+      }
+    }
+  }
 
-  def action(state: State, number: Int): GameController => Unit = ???
+  def action(state: State, number: Int): GameController => Unit = state match {
+    case GiveUp          => GameController => GameController.giveUp(number)
+    case WrongInput      => GameController => GameController.wrongInput()
+    case NumberIsBigger  => GameController => GameController.numberIsBigger()
+    case NumberIsSmaller => GameController => GameController.numberIsSmaller()
+    case Guessed         => GameController => GameController.guessed()
+  }
 
-  def completed(state: State): Boolean = ???
+  def completed(state: State): Boolean = state == GiveUp || state == Guessed
 }
