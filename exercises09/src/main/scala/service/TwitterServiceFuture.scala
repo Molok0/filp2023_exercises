8b44535ef74d968a31e7efa1d3c5ed9b34f1cc98
DmitryProshin <do.proshin@yandex.ru>
2023-04-25 14:43:46 +0500

    Прошин Дмитрий exercises09

--- exercises09/src/main/scala/service/TwitterServiceFuture.scala
+++ exercises09/src/main/scala/service/TwitterServiceFuture.scala
@@ -1,20 +1,51 @@
 package service
 
+import service.domain.GetTweetResponse.{Found, NotFound}
 import service.domain.{GetTweetResponse, GetTweetsResponse}
 import twitter.TwitterApi
 import twitter.domain.TwitterError.{LikeAlreadyExistError, LikeNotExistError, TweetNotExistError}
-import twitter.domain.{TweetId, User}
+import twitter.domain.{TweetId, TweetInfo, User}
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 class TwitterServiceFuture(api: TwitterApi)(implicit ec: ExecutionContext) extends TwitterService[Future] {
-  def tweet(user: User, text: String): Future[TweetId] = ???
+  def tweet(user: User, text: String): Future[TweetId] = {
+    val promise = Promise[TweetId]
+    Future(api.tweet(user, text)(promise.complete))
+    promise.future
+  }
 
-  def like(user: User, tweetId: TweetId): Future[Unit] = ???
+  def like(user: User, tweetId: TweetId): Future[Unit] = {
+    val promise = Promise[Unit]
+    Future(api.like(user, tweetId)(promise.complete))
+    promise.future.recover({ case LikeAlreadyExistError => () })
+  }
 
-  def unlike(user: User, tweetId: TweetId): Future[Unit] = ???
+  def unlike(user: User, tweetId: TweetId): Future[Unit] = {
+    val promise = Promise[Unit]
+    Future(api.unlike(user, tweetId)(promise.complete))
+    promise.future.recover({ case LikeNotExistError => () })
+  }
 
-  def getTweet(tweetId: TweetId): Future[GetTweetResponse] = ???
+  def getTweet(tweetId: TweetId): Future[GetTweetResponse] = {
+    val promise = Promise[TweetInfo]
+    Future(api.get(tweetId)(promise.complete))
+    promise.future.transform {
+      case Success(x) => Success(Found(x))
+      case Failure(e) => Success(NotFound(tweetId))
+    }
+  }
 
-  def getTweets(ids: List[TweetId]): Future[GetTweetsResponse] = ???
+  def getTweets(ids: List[TweetId]): Future[GetTweetsResponse] = {
+    Future
+      .traverse(ids)(getTweet)
+      .map(_.foldLeft(GetTweetsResponse(Set.empty[TweetId], Set.empty[TweetInfo]))((tweets, response) => {
+        response match {
+          case Found(v)    => GetTweetsResponse(tweets.notFound, tweets.found + v)
+          case NotFound(v) => GetTweetsResponse(tweets.notFound + v, tweets.found)
+        }
+      }))
+  }
+
 }
--- exercises09/src/main/scala/service/TwitterServiceIO.scala
+++ exercises09/src/main/scala/service/TwitterServiceIO.scala
@@ -2,22 +2,50 @@ package service
 
 import cats.effect.IO
 import cats.syntax.all._
-
+import service.domain.GetTweetResponse.{Found, NotFound}
 import service.domain._
-
 import twitter.domain.TwitterError._
 import twitter.TwitterApi
 import twitter.domain._
 
+import scala.util.{Failure, Success}
+
 // Воспользуйтесь синтаксисом map, recover, traverse из cats.syntax.all_
 class TwitterServiceIO(api: TwitterApi) extends TwitterService[IO] {
-  def tweet(user: User, text: String): IO[TweetId] = ???
+  def tweet(user: User, text: String): IO[TweetId] = IO.async_(cb => api.tweet(user, text)(x => cb(x.toEither)))
 
-  def like(user: User, tweetId: TweetId): IO[Unit] = ???
+  def like(user: User, tweetId: TweetId): IO[Unit] =
+    IO.async_((cb: Either[Throwable, Unit] => Unit) => api.like(user, tweetId)(x => cb(x.toEither)))
+      .recover({ case LikeAlreadyExistError => () })
 
-  def unlike(user: User, tweetId: TweetId): IO[Unit] = ???
+  def unlike(user: User, tweetId: TweetId): IO[Unit] =
+    IO.async_((cb: Either[Throwable, Unit] => Unit) => api.unlike(user, tweetId)(x => cb(x.toEither)))
+      .recover({
+        case LikeNotExistError => ()
+      })
 
-  def getTweet(tweetId: TweetId): IO[GetTweetResponse] = ???
+  def getTweet(tweetId: TweetId): IO[GetTweetResponse] = {
+    IO.async_((cb: Either[Throwable, GetTweetResponse] => Unit) =>
+      api.get(tweetId)(x =>
+        cb((x match {
+          case Failure(_) => Success(NotFound(tweetId))
+          case Success(value)     => Success(Found(value))
+        }).toEither)
+      )
+    ).recover(PartialFunction.empty)
+  }
 
-  def getTweets(ids: List[TweetId]): IO[GetTweetsResponse] = ???
+  def getTweets(ids: List[TweetId]): IO[GetTweetsResponse] = {
+    ids
+      .traverse(getTweet)
+      .map(list =>
+        list.foldLeft(GetTweetsResponse(Set.empty[TweetId], Set.empty[TweetInfo])) { (tweets, response) =>
+          response match {
+            case Found(v)    => GetTweetsResponse(tweets.notFound, tweets.found + v)
+            case NotFound(v) => GetTweetsResponse(tweets.notFound + v, tweets.found)
+          }
+        }
+      )
+  }
+
 }
